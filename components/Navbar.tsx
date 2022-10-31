e9b1cf30ac9f094a9568a2a0fa189f7ba2e54f65
Dahlia Foo <dahliafoo@gmail.com>
2022-11-01 06:55:17 +1100

    styled video card

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
 
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
+import { createOrGetUser } from '../utils'
+
+import useAuthStore from '../store/authStore'
+
 const Navbar = () => {
+  const { userProfile, addUser, removeUser } = useAuthStore()
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -16,6 +21,46 @@ const Navbar = () => {
           <Image className="cursor-pointer" src={Logo} alt="TikTik" />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {userProfile ? (
+          <div className="flex gap-5 items-center">
+            <Link href="/upload">
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <Image
+                  width="40"
+                  height="40"
+                  className="rounded-full cursor-pointer"
+                  src={userProfile.image}
+                  alt="profile photo"
+                />
+              </Link>
+            )}
+            <button
+              type="button"
+              onClick={() => {
+                googleLogout()
+                removeUser()
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => {
+              console.log(`Error`)
+            }}
+          />
+        )}
+      </div>
     </div>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -36,30 +36,7 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className="px-2 py-4 hidden xl:block">
-              <p className="text-gray-400">
-                Log in to comment and like on videos
-              </p>
-              <div className="pr-4">
-                <GoogleLogin
-                  clientId=""
-                  render={(renderProps) => (
-                    <button
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                      className="bg-white text-lg text-[#F51997] border-[1px] border-[#f51997] px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#f51997] cursor-pointer"
-                    >
-                      Log In
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy="single_host_origin"
-                />
-              </div>
-            </div>
-          )}
+
           <Discover />
           <SuggestedAccounts />
           <Footer />
